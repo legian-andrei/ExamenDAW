f5e659c0b196423c091dd4537be87a8944bd97af
legian-andrei <andreilegian138@yahoo.com>
2024-01-24 19:55:12 +0200

    initCommit

---
+++ backend/backend/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext() : base()
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true");
+		}
+
+		// creare clasa Student.cs in models
+		//	public DbSet<Student> Students { get; set; }
+		// NuGet package -> package manager console
+		// Add-Migration CreateStudent
+		// Update-Database
+	}
+}
